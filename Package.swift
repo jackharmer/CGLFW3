cd64c2549ddc110ba3b9d79e7d416d84929da5ed
Jack harmer <jack2harmer@gmail.com>
2020-01-11 23:29:05 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,5 +3,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "CGLFW3"
+    name: "CGLFW3",
+    products: [
+        .systemLibrary(
+            name: "CGLFW3",
+            path: "",
+            pkgConfig: "glfw3",
+            providers: [
+                .brew(["--HEAD git glfw3"]),
+                .apt(["libglfw3", "libglfw3-dev"])
+            ])
+    ]
 )
