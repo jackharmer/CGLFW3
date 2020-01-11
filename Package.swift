3888c2de6b2ae27763122644734ff20cdbf72719
Jack harmer <jack2harmer@gmail.com>
2020-01-11 23:36:55 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "CGLFW3",
     products: [
-        .library(name: "CGLFW3", targets: ["CGLFW3"]),
+        .library(name: "CGLFW3", targets: ["CGLFW3"])
+    ],
     targets: [
         .systemLibrary(
             name: "CGLFW3",
